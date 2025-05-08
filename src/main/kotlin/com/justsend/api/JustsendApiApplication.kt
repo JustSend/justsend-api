851337ddb146b42522ffbddd4397e2ac89b95464
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-08 19:14:36 -0300

    init

---
+++ src/main/kotlin/com/justsend/api/JustsendApiApplication.kt
@@ -0,0 +1,11 @@
+package com.justsend.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JustsendApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<JustsendApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/justsend/api/JustsendApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.justsend.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JustsendApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
