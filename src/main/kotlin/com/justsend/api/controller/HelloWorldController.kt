683476b08e5d985422efcee66bec0d0f0353729e
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-09 11:12:49 -0300

    feat: basic helloWorld endpoint

---
+++ src/main/kotlin/com/justsend/api/controller/HelloWorldController.kt
@@ -0,0 +1,11 @@
+package com.justsend.api.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    fun helloWorld() = "Hello World!"
+}
