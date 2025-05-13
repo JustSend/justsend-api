c9edc82c0ec435f99e6013e17a72b4c3a02cbd74
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-13 15:00:56 -0300

    test(Wallet): enhance test cases with descriptive names and additional scenarios

--- src/test/kotlin/com/justsend/api/WalletTests.kt
+++ src/test/kotlin/com/justsend/api/WalletTests.kt
@@ -6,53 +6,55 @@ import kotlin.test.Test
 
 class WalletTests {
 
+  val wallet = Wallet()
+
   @Test
-  fun `new wallet should start with no balance`() {
-    val wallet = Wallet()
+  fun `new wallet should start with no balance - 01`() {
     assert(wallet.getBalance() == 0)
   }
 
   @Test
-  fun `adding positive balance to the wallet the wallet balance should not be cero`() {
-    val wallet = Wallet()
+  fun `adding positive balance to the wallet the wallet balance should not be cero - 02`() {
     wallet.addBalance(10)
     assert(wallet.getBalance() != 0)
   }
 
   @Test
-  fun `adding positive balance to wallet the wallet should have the new balance`() {
-    val wallet = Wallet()
+  fun `adding positive balance to wallet the wallet should have the new balance - 03`() {
     wallet.addBalance(10)
     assert(wallet.getBalance() != 0)
   }
 
   @Test
-  fun `adding negative balance to wallet should throw exception`() {
-    val wallet = Wallet()
-
+  fun `adding negative balance to wallet should throw exception - 04`() {
     assertThrows(IllegalArgumentException::class.java) {
       wallet.addBalance(-10)
     }
   }
 
   @Test
-  fun `removing 0 balance should be the same`() {
-    val wallet = Wallet()
+  fun `removing 0 balance should be the same - 05`() {
     wallet.addBalance(10)
     wallet.removeBalance(0)
     assert(wallet.getBalance() == 10)
   }
 
   @Test
-  fun `removing negative balance throw exception`() {
-    val wallet = Wallet()
+  fun `removing negative balance throw exception - 06`() {
     assertThrows(IllegalArgumentException::class.java) {
       wallet.removeBalance(-10)
     }
   }
 
   @Test
-  fun `removing more balance than actual should throw error`() {
+  fun `removing same balance as current balance should leave wallet with no balance - 07`() {
+    wallet.addBalance(10)
+    wallet.removeBalance(10)
+    assert(wallet.getBalance() == 0)
+  }
+
+  @Test
+  fun `removing more balance than actual should throw error - 08`() {
     val wallet = Wallet()
     wallet.addBalance(10)
     assertThrows(IllegalArgumentException::class.java) {
