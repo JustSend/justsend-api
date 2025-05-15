6804849f740d54ada43cdcc735d179b9c0b5314f
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-15 15:59:35 -0300

    refactor(Wallet): change Wallet to immutable structure and update add/remove methods to return new instances

--- src/main/kotlin/com/justsend/api/model/Wallet.kt
+++ src/main/kotlin/com/justsend/api/model/Wallet.kt
@@ -1,23 +1,35 @@
 package com.justsend.api.model
 
-class Wallet() {
+class Wallet private constructor(
+  private val balances: Map<Currency, Amount>
+) {
 
-  private var balances = mutableMapOf<Currency, Amount>()
+  constructor() : this(emptyMap())
 
-  fun add(money: Money) {
+  fun add(money: Money): Wallet {
     require(money.amount >= 0.0) { "Amount cannot be negative" }
-    balances[money.currency] = balances.getOrDefault(money.currency, 0.0) + money.amount
+    val newBalances = balances.toMutableMap()
+    val updatedAmount = newBalances.getOrDefault(money.currency, 0.0) + money.amount
+    newBalances[money.currency] = updatedAmount
+    return Wallet(newBalances)
   }
 
-  fun remove(money: Money) {
+  fun remove(money: Money): Wallet {
     require(money.amount >= 0.0) { "Amount cannot be negative" }
     val currentAmount = balances.getOrDefault(money.currency, 0.0)
     require(currentAmount >= money.amount) {
       "Insufficient balance: trying to remove ${money.amount} from $currentAmount ${money.currency}"
     }
-    balances[money.currency] = currentAmount - money.amount
+    val newBalances = balances.toMutableMap()
+    val updatedAmount = currentAmount - money.amount
+    if (updatedAmount == 0.0) {
+      newBalances.remove(money.currency)
+    } else {
+      newBalances[money.currency] = updatedAmount
+    }
+    return Wallet(newBalances)
   }
 
-  fun getBalanceIn(currency: Currency): Double = balances.getOrDefault(currency, 0.0)
+  fun getBalanceIn(currency: Currency): Amount = balances.getOrDefault(currency, 0.0)
   fun getAllBalances(): Map<Currency, Amount> = balances.toMap()
 }
