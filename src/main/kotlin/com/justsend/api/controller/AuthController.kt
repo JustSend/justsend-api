577794e8625b94595e3fa96c491fdfcb12d9adb6
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-22 20:13:06 -0300

    fix(AuthController): update RestController annotation to use RequestMapping for proper routing

--- src/main/kotlin/com/justsend/api/controller/AuthController.kt
+++ src/main/kotlin/com/justsend/api/controller/AuthController.kt
@@ -5,9 +5,11 @@ import com.justsend.api.service.AuthService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/auth")
+@RestController
+@RequestMapping("/auth")
 class AuthController(
   private val authService: AuthService
 ) {
