5443e87f26ee93f215901302e5aa3f5af9d7808e
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-23 19:46:22 -0300

    fix(wallet): correct RestController annotation for wallet endpoints

--- src/main/kotlin/com/justsend/api/controller/WalletController.kt
+++ src/main/kotlin/com/justsend/api/controller/WalletController.kt
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import kotlin.fold
 
-@RestController("/wallet")
+@RestController()
+@RequestMapping("/wallet")
 class WalletController(
   val walletService: WalletService
 ) {
