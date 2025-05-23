555c1735636fcbd7d311c54bd256a2bdbdc7853f
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-23 17:47:53 -0300

    feat(wallet): add basic endpoints

--- src/main/kotlin/com/justsend/api/controller/WalletController.kt
+++ src/main/kotlin/com/justsend/api/controller/WalletController.kt
@@ -1,11 +1,22 @@
 package com.justsend.api.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/wallet")
 class WalletController {
 
-  @PostMapping("/create")
-  fun createWallet() {}
+  @GetMapping
+  fun getWallet() {}
+
+  @GetMapping("/{currency}")
+  fun getWalletBalanceInCurrency(@PathVariable currency: String) {}
+
+  @PostMapping("/add")
+  fun addMoney() {}
+
+  @PostMapping("/remove")
+  fun removeMoney() {}
 }
